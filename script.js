3c3760f298273ebe0fb6c3f35f9fe67fd0a89be0
jashwanthreddy2508-commits <jashwanthreddy2508@gmail.com>
2026-08-21 11:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 function createTable() {
-    //Write your code here
-  
+    let rows = Number(prompt("Input number of rows"));
+    let col = Number(prompt("Input number of columns"));
+
+    let table = document.getElementById("myTable");
+
+    for (let i = 0; i <= rows; i++) {
+
+        let tr = document.createElement("tr");
+
+        for (let j = 0; j <= col; j++) {
+
+            if (i === 0) {
+                let th = document.createElement("th");
+
+                th.textContent = `Row-${i} Column-${j}`;
+
+                tr.appendChild(th);
+            } 
+            else {
+                let td = document.createElement("td");
+
+                td.textContent = `Row-${i} Column-${j}`;
+
+                tr.appendChild(td);
+            }
+        }
+
+        table.appendChild(tr);
+    }
 }
